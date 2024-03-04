da374f73749a685f3d4494b52060d9db050a8fd3
FengGaoCSC <90297088+FengGaoCSC@users.noreply.github.com>
2024-03-04 13:49:41 +0800

    Update daemon_main.go
    Signed-off-by: FengGaoCSC <90297088+FengGaoCSC@users.noreply.github.com>

--- daemon/cmd/daemon_main.go
+++ daemon/cmd/daemon_main.go
@@ -393,8 +393,8 @@ func InitGlobalFlags(cmd *cobra.Command, vp *viper.Viper) {
 	flags.Duration(option.WireguardPersistentKeepalive, 0, "The Wireguard keepalive interval as a Go duration string")
 	option.BindEnv(vp, option.WireguardPersistentKeepalive)
 
-	flags.Bool(option.EnableWireguardHubmode, false, "Enable WireGuard hub spoke mode")
-	option.BindEnv(vp, option.EnableWireguardHubmode)
+	flags.String(option.WireguardTopology, "", "WireGuard point-to-point mode or hub-spoke mode")
+	option.BindEnv(vp, option.WireguardTopology)
 
 	flags.String(option.NodeEncryptionOptOutLabels, defaults.NodeEncryptionOptOutLabels, "Label selector for nodes which will opt-out of node-to-node encryption")
 	option.BindEnv(vp, option.NodeEncryptionOptOutLabels)
